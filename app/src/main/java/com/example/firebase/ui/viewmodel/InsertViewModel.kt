1319c9e5cf6e53639569c60da214c20c078286e3
nauvalachid <nauvalachid@gmail.com>
2025-01-12 20:35:32 +0700

    merubah nama val jenisKelamin menjadi gender,Menambahkan fungsi ekstensi untuk mengonversi MahasiswaEvent ke dalam model Mahasiswa,Menambahkan fungsi untuk menyimpan data Mahasiswa setelah validasi dan menangani status form

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -4,7 +4,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebase.model.Mahasiswa
 import com.example.firebase.repository.RepositoryMhs
+import kotlinx.coroutines.launch
 
 class InsertViewModel (
     private val mhs: RepositoryMhs
@@ -27,13 +30,29 @@ class InsertViewModel (
         val errorState = FormErrorState(
             nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
             nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
-            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            gender = if (event.gender.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
             alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
             kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
         )
 
         uiEvent = uiEvent.copy(isEntryValid = errorState)
         return errorState.isValid()
+    }
+
+    fun insertMhs() {
+        if (validataFields()) {
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    mhs.insertMhs(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data berhasil disimpan")
+                } catch (e: Exception) {
+                    uiState = FormState.Error("Data gagal disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data tidak valid")
+        }
     }
 }
 
@@ -52,22 +71,33 @@ data class InsertUiState (
 data class FormErrorState(
     val nim: String? = null,
     val nama: String? = null,
-    val jenisKelamin: String? = null,
+    val gender: String? = null,
     val alamat: String? = null,
     val kelas: String? = null,
     val angkatan: String? = null
 ) {
     fun isValid(): Boolean {
-        return nim == null && nama == null && jenisKelamin == null &&
+        return nim == null && nama == null && gender == null &&
                 alamat == null && kelas == null && angkatan == null
     }
 }
 
+//data class Variabel yang menyimpan data input form
 data class MahasiswaEvent(
     val nim: String = "",
     val nama: String = "",
-    val jenisKelamin: String = "",
+    val gender: String = "",
     val alamat: String = "",
     val kelas: String = "",
     val angkatan: String = ""
+)
+
+//Menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel () : Mahasiswa = Mahasiswa (
+    nim = nim,
+    nama = nama,
+    gender = gender,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan,
 )
