a990fc68d06b288208af9a2ef5def55e3baecae0
nauvalachid <nauvalachid@gmail.com>
2025-01-12 19:58:21 +0700

    Menambahkan data class FormErrorState untuk validasi form input data Mahasiswa

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -9,6 +9,20 @@ class InsertViewModel (
 
 }
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 data class MahasiswaEvent(
     val nim: String = "",
     val nama: String = "",
